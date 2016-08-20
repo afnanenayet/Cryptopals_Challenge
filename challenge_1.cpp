7f06ca1143b96f001338a1f21b6ffcc8af754b30
Afnan Enayet <afnan.enayet@gmail.com>
2016-08-19 23:50:33 -0400

    Finished hex_to_base64 function

--- challenge_1.cpp
+++ challenge_1.cpp
@@ -2,60 +2,57 @@
 #include <bitset>
 #include <iostream>
 #include <queue>
+#include <stdexcept>
 
 namespace crypto {
-	// Converts a hex encoded string to a base64 encoded string
-	// with bit level manipulations
-
 	// Converts a hex char to a decimal representation
-	// cast as a char
-	inline char hex_to_decimal(char hex_char) {
+	// cast as an unsigned char
+	inline unsigned char hex_to_decimal(unsigned char hex_char) {
 		// Getting the index of the character offset
 		// WARNING: the character ranges may NOT be consecutive
 		// depending on the encoding used (should work in
 		// UTF-8 and ASCII)
 		if ('0' <= hex_char && hex_char <= '9') {
-			return hex_char - '0';
+			return (hex_char - '0');
 		}
+
 		else if ('A' <= hex_char && hex_char <= 'F') {
 			return (hex_char - 'A') + 0xA;
 		}
+
 		else if ('a' <= hex_char && hex_char <= 'f') {
 			return (hex_char - 'a') + 0xA;
 		}
+
 		else {
-			throw - 1;
+			throw std::invalid_argument("Received a character that is not a valid hexadecimal unsigned char")
 		}
 	}
 
 	std::string hex_to_base64(std::string hex_string) {
 		const std::string base64_cipher =
 			"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
-		const size_t array_length = 4;
+		const size_t array_length = 2;
 		std::string base64_string;
-		base64_string.reserve((hex_string.length() * 4) / 6);
 
+        // 4 bytes / 6 bytes reduces to 2 / 3
+		base64_string.reserve((hex_string.length() * 2) / 3);
+        
 		auto valid_words = 1;
 
-		// Turning three hex letters into a 24 bit number then
-		// dividing up into 4 base64 characters (6 bits each)
-		// 24 is evenly divisible by 8 (size of a char), 4 (size
+		// Turning three hex letters into a 12 bit number then
+		// dividing up into 2 base64 characters (6 bits each)
+		// 12 is evenly divisible by 4 (size
 		// of a hex word), and 6 (size of a base64 word)
 		for (int i = 0; i < hex_string.length(); i += 3) {
-			unsigned char base64_words[array_length];
-
-			// using '=' as null indicator as 0 is a valid
-			// base64 index
-			std::fill_n(base64_words, array_length, '=');
-
 			int hex_word_b =
-				crypto::hex_to_decimal(hex_string.at(i)) << 16;
+				crypto::hex_to_decimal(hex_string.at(i)) << 8;
 
 			// Checking to see if there are any words afterwards,
-			// if not, then pad with 0's
+			// if not, then pad with ='s
 			if (i + 1 < hex_string.length()) {
 				hex_word_b +=
-					crypto::hex_to_decimal(hex_string.at(i + 1)) << 8;
+					crypto::hex_to_decimal(hex_string.at(i + 1)) << 4;
 				valid_words += 1;
 			}
 
@@ -64,29 +61,28 @@ namespace crypto {
 					crypto::hex_to_decimal(hex_string.at(i + 2));
 				valid_words += 1;
 			}
-
+            
 			// Unpacking into four base64 words
 			// and masking first 6 bits
 			// and shifting as necessary
-			for (int n = 0; n < ((valid_words * 8) / 6) + 1; n++) {
-				unsigned char current_char = hex_word_b >> (6 * n) & 0b111111;
-				base64_words[n] = current_char;
+			for (int n = (valid_words * 4 > 6) ? 2 : 1; n > 0; n--) {
+                base64_string.push_back(base64_cipher
+				.at(hex_word_b >> (6 * (n - 1)) & 0b111111;));
 			}
+        }
 
-			// Converting each unpacked 6 bit word to the corresponding
-			// base64 character
-			for (auto character : base64_words) {
-				base64_string.push_back(base64_cipher.at(character));
-			}
-		}
+        // Adding = padding for base64 string
+        // Base64 can only guarantee even measuring in three byte bundles
+        for (int i = 0; i < ((hex_string.length() * 4) / 8) % 3; i++) {
+            base64_string.push_back('=');
+        }
 		return base64_string;
 	}
 }
 
 using namespace std;
 int main() {
-	cout << crypto::hex_to_base64("13f") << "\n";
-	// should be "ERER"
-	system("PAUSE");
+	cout << crypto::hex_to_base64("12") << "\n";
+    // Correct hex conversion is: ""
 	return 0;
 }
